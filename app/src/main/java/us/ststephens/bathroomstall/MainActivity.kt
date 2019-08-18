96e9f38d6756fc3d9f12519e83161ecbb9a02c7d
Scott Stephens <scott.stephens@vauto.com>
2019-08-18 14:19:10 -0500

    added location fetching

--- app/src/main/java/us/ststephens/bathroomstall/MainActivity.kt
+++ app/src/main/java/us/ststephens/bathroomstall/MainActivity.kt
@@ -1,6 +1,14 @@
 package us.ststephens.bathroomstall
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.HandlerThread
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -9,20 +17,27 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ViewSwitcher
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 import us.ststephens.bathroomstall.net.NetworkState
 import java.lang.Exception
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: NoteListViewModel
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var noteEntryBox: EditText? = null
     private var swipeRefreshLayout: SwipeRefreshLayout? = null
     private var viewSwitcher: ViewSwitcher? = null
@@ -32,10 +47,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         viewModel = ViewModelProviders.of(this)[NoteListViewModel::class.java]
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         noteEntryBox = findViewById(R.id.note_entry_box)
         swipeRefreshLayout = findViewById(R.id.swiperefresh)
         viewSwitcher = findViewById(R.id.send_control_switcher)
         viewSwitcher?.displayedChild = 0
+
 
         findViewById<ImageView>(R.id.send_button).let { sendButton ->
             toggleSendButton(sendButton, noteEntryBox?.text?.isNotEmpty() == true)
@@ -53,6 +70,25 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel.getNotes().observe(this, noteListObserver)
+
+        viewModel.isLocationServiceAvailable.observe(this, Observer { isAvailable ->
+            Log.d("Stall", "Location Available? $isAvailable")
+        })
+
+        viewModel.lastKnownLocation.observe(this, Observer { location ->
+            Snackbar.make(findViewById(R.id.coordinator), "Location recorded: $location", Snackbar.LENGTH_INDEFINITE).show()
+            Log.d("Stall", "Location recorded: $location")
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
     }
 
     private fun messageAdded(messageId: String) {
@@ -79,6 +115,68 @@ class MainActivity : AppCompatActivity() {
     private fun toggleSendButton(view: View, isEnabled: Boolean) {
         view.isEnabled = isEnabled
         view.alpha = if (isEnabled) 1f else .65f
+    }
+
+    private fun startLocationUpdates() {
+        requestLocationPermission()
+    }
+
+    private fun requestLocationPermission() {
+        val permission =  Manifest.permission.ACCESS_FINE_LOCATION
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                //TODO show the reason why we need their location
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(permission), REQ_LOCATION_PERMISSION)
+            }
+        } else {
+            fetchLocationUpdates()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            REQ_LOCATION_PERMISSION -> if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+                fetchLocationUpdates()
+            } else {
+                //todo: Let the user know they can't use the app until they allow the location permission
+            }
+        }
+    }
+
+    @Throws(SecurityException::class)
+    private fun fetchLocationUpdates() {
+        viewModel.locationRequest?.let { request ->
+            checkLocationServices()
+                .addOnSuccessListener {
+                    fusedLocationClient.requestLocationUpdates(request, viewModel.locationCallback, Looper.myLooper())
+                }
+                .addOnFailureListener { exception ->
+                    if (exception is ResolvableApiException) try {
+                        exception.startResolutionForResult(this, REQ_CHECK_SETTINGS) //todo handle the result
+                    } catch (sendEx: IntentSender.SendIntentException) {
+                        //todo: Let the user know they can't use the app because they don't support our location update
+                    }
+                }
+        } //todo: Let the user know they can't use the app because they don't support our location update
+
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(viewModel.locationCallback)
+    }
+
+    private fun checkLocationServices() : Task<LocationSettingsResponse> {
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        return client.checkLocationSettings(viewModel.locationSettingsRequest)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQ_CHECK_SETTINGS) {
+            startLocationUpdates()
+        }
     }
 
     private val postNoteObserver = Observer<NetworkState<DocumentReference>> { state ->
@@ -118,6 +216,11 @@ class MainActivity : AppCompatActivity() {
             }
             is NetworkState.Error -> swipeRefreshLayout?.isRefreshing = false
         }
+    }
+
+    companion object {
+        private const val REQ_CHECK_SETTINGS = 5
+        private const val REQ_LOCATION_PERMISSION = 6
     }
 }
 
--- app/src/main/java/us/ststephens/bathroomstall/NoteListViewModel.kt
+++ app/src/main/java/us/ststephens/bathroomstall/NoteListViewModel.kt
@@ -1,16 +1,50 @@
 package us.ststephens.bathroomstall
 
+import android.location.Location
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.*
 import com.google.firebase.firestore.*
 import us.ststephens.bathroomstall.net.NetworkState
 
+const val INTERVAL = 1000L * 60
+const val MIN_INTERVAL = 1000L * 15
+const val MIN_DISTANCE_FOR_UPDATE = 0f
+const val LOCATION_PRIORITY = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
 class NoteListViewModel : ViewModel() {
 
     private val db = FirebaseFirestore.getInstance()
     val adapter = NoteListAdapter()
     private var queryRegistration: ListenerRegistration? = null
+
+    //location variables
+    private val updatableLocation = MutableLiveData<Location?>()
+    val lastKnownLocation: LiveData<Location?> = updatableLocation
+
+    private val updatableLocationAvailability = MutableLiveData<Boolean>()
+    val isLocationServiceAvailable: LiveData<Boolean> = updatableLocationAvailability
+
+    val locationRequest: LocationRequest? = LocationRequest.create()?.apply {
+        interval = INTERVAL
+        fastestInterval = MIN_INTERVAL
+        smallestDisplacement = MIN_DISTANCE_FOR_UPDATE
+        priority = LOCATION_PRIORITY
+    }
+
+    val locationSettingsRequest: LocationSettingsRequest? = locationRequest?.let { locationRequest ->
+        LocationSettingsRequest.Builder().addLocationRequest(locationRequest).build()
+    }
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(results: LocationResult?) {
+            updatableLocation.value = results?.lastLocation
+        }
+
+        override fun onLocationAvailability(locationAvailability: LocationAvailability?) {
+            updatableLocationAvailability.value = locationAvailability?.isLocationAvailable == true
+        }
+    }
 
     fun postNote(message: String) : LiveData<NetworkState<DocumentReference>> {
         val liveData = MutableLiveData<NetworkState<DocumentReference>>()
